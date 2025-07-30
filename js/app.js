b2878022fdf34fcc2a17920d892fac770cea7c47
rahmatillo <rahmatillo>
2025-07-30 13:30:45 +0500

    app js

---
+++ js/app.js
@@ -0,0 +1,196 @@
+// let Number = prompt("");
+
+// if (Number == "10") {
+//   console.log("To'g'ri son!");
+// } else {
+//   console.log("Boshqa son kiritdingiz!");
+// }
+
+// let ha = prompt("Javobni kiriting (ha,yo'q)");
+
+// if (ha == "ha") {
+//   console.log("Ruxsat bor!");
+// } else if (ha > "ha") {
+//   console.log("Ruxsat yo'q!");
+// }
+
+// let input = prompt("0 dan 100 gacha bo'lgan son kiriting:");
+
+// let number = Number(input);
+
+// if (number >= 0 && number <= 100) {
+//   console.log("Yaxshi diapazon!");
+// } else {
+//   console.log("Diapazon tashqarisida!");
+// }
+
+// let soz = prompt("Ismingizni kiriting: ");
+
+// if (soz.length > 5) {
+//   console.log("Uzoq so'z!");
+// } else {
+//   console.log("Qisqa so'z!");
+// }
+
+// let son = prompt("biror son kiriting:");
+
+// if (son) {
+//   if (son % 2 === 0) {
+//     console.log("Juft son!");
+//   } else {
+//     console.log("Toq son!");
+//   }
+// }
+
+// let num = prompt("so'z kiriting:");
+
+// if (num) {
+//   if (num === "salom") {
+//     console.log("Salom, dunyo!");
+//   } else {
+//      console.log("Boshqa so'z kiritdingiz!");
+//   }
+// }
+
+// 11;
+// 12;
+
+// let input = prompt("son kiriting");
+
+// let number = Number(input);
+
+// if (number >= "10" && number <= "100") {
+//    console.log("Chegaradan ichida!");
+// } else {
+//    console.log("Chegara tashqarida!");
+// }
+
+// let name = prompt("Biron so'z kirit kiriting: ");
+
+// if (name == "") {
+//   console.log("Hech narsa kiritmadingiz!");
+// } else {
+//   console.log("So'z kiritdingiz!");
+// }
+
+// let son1 = prompt("1-sonni kiriting:");
+// let son2 = prompt("2-sonni kiriting:");
+
+// son1 = Number(son1);
+// son2 = Number(son2);
+
+// if (son1 == son2) {
+//    console.log("Sonlar teng!");
+// } else {
+//    console.log("Sonlar teng emas!");
+// }
+
+// let son = prompt("biror son kiriting:");
+
+// if (son) {
+//   if (son >= 50 && son % 2 == 0) {
+//      console.log("Juft son!");
+//   } else {
+//      console.log("Toq son!");
+//   }
+// }
+
+// let num = prompt("so'z kiriting:");
+
+// if (num) {
+//   if (num === "java script" || num === "codding") {
+//      console.log("Siz dasturchisiz!");
+//   } else {
+//      console.log("Boshqa so'z!");
+//   }
+// }
+
+// let num = prompt("Son kiriting: ");
+
+// if (num == "0") {
+//   console.log("Bu son 0");
+// } else if (num > "0") {
+//   console.log("Son nol emas!");
+// }
+
+// let input = prompt("son kiriting");
+
+// let number = Number(input);
+
+// if (number >= "13" && number <= "19") {
+//    console.log("Siz o'smirsiz!");
+// } else {
+//    console.log("O'smir emassiz!");
+// }
+
+// let soz = prompt("so'z kiriting: ");
+
+// if (soz.length < 3)
+//   if (soz.length == "") {
+//     console.log("uzoq so'z!");
+//   } else {
+//     console.log("qisqa so'z!");
+//   }
+
+// 21
+
+// let age = prompt("Yoshingizni kirirting: ");
+
+// if (age >= "18" || age === "erkak") {
+//    console.log("Siz armiyaga borasiz!");
+// } else {
+//    console.log("Armiyaga bormaysiz!");
+// }
+
+// let input = prompt("son kiriting");
+
+// let number = Number(input);
+
+// if ((number <= "10" && number >= "100") || number % 2 == 1) {
+//    console.log("Chegaradagi toq son!");
+// } else {
+//    console.log("boshqa son");
+// }
+
+// let num = prompt("so'z kiriting:");
+
+// if (num) {
+//   if (num === "admin") {
+//      console.log("xush kelibsiz");
+//   } else {
+//      console.log("siz admin emassiz");
+//   }
+// }
+
+// let input = prompt("tahminiy son kirit");
+
+// let number = Number(input);
+
+// if ((number >= 0 && number <= 10) || (number >= 100 && number <= 200)) {
+//    console.log("Maxsus diapazon!");
+// } else {
+//    console.log("Oddiy son!");
+// }
+
+// let input = prompt("0 dan 100 gacha bo'lgan son kiriting:");
+
+// let number = Number(input);
+
+// if (input > 0) {
+//   if (input % 2 == 0 || input > 100) {
+//     console.log("Maxsus son");
+//   }
+// } else {
+//   console.log("Maxus son emas");
+// }
+
+// let soz1 = prompt("1-so'zni kiriting:");
+// let soz2 = prompt("2-so'zni kiriting:");
+
+// let soz1Lower = soz1.toLowerCase();
+
+// if (soz1.length > soz2.length || soz1Lower === "salom") {
+//   alert("Birinchi so'z g'olib!");
+// } else {
+//   alert("Ikkinchi so'z g'olib!");
+// }
